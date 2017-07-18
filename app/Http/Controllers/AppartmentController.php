e9b3fb43a96833ec2439b9787a2685fd6eb9cd8c
fsgom <fernandosegom@gmail.com>
2017-07-17 21:30:13 -0500

    Init project

---
+++ app/Http/Controllers/AppartmentController.php
@@ -0,0 +1,15 @@
+<?php
+  namespace App\Http\Controllers;
+
+  class AppartmentController extends Controller
+  {
+    public function __construct(){
+
+    }
+
+    public function index(){
+
+    }
+
+
+  }
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Rentoocomparto Server
+
+Personal project for handle request from a external client (made with ReactJS).
+In a few days this readme should be more complete
