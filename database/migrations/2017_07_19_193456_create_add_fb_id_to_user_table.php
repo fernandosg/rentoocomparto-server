19f8e8cb117f41ef4137481997b76f247d4a96bd
fernandosg <fsg_tmp@hotmail.com>
2017-07-19 14:48:34 -0500

    Added fb_id to user model

---
+++ database/migrations/2017_07_19_193456_create_add_fb_id_to_user_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddFbIdToUserTable extends Migration
+{
+
+    public function up()
+    {
+      Schema::table('users', function($table) {
+          $table->string('fb_id');
+      });
+    }
+
+    public function down()
+    {
+      Schema::table('users', function($table) {
+          $table->dropColumn('fb_id');
+      });
+    }
+}
